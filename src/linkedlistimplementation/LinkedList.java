d2c02a2995f475957706596100f36adf9e4b3e50
AV-NASH <avinash21213434@gmail.com>
2020-10-16 23:02:21 +0530

    Added method to add element to linked list

--- src/linkedlistimplementation/LinkedList.java
+++ src/linkedlistimplementation/LinkedList.java
@@ -1,5 +1,27 @@
 package linkedlistimplementation;
 
-public class LinkedList {
-Node head=null;
+public class LinkedList<E> {
+    Node head=null;
+
+    public void add(E data){
+        Node node=new Node(data);
+
+        if(head==null)
+            head=node;
+        else{
+            node.next=head;
+            head=node;
+        }
+    }
+    public void printList(){
+        if(head==null) System.out.println("LinkedList empty");
+        else{
+        Node node=head;
+        while(node!=null){
+            System.out.println(node.data);
+            node=node.next;
+        }
+
+        }
+    }
 }
--- src/linkedlistimplementation/LinkedListTest.java
+++ src/linkedlistimplementation/LinkedListTest.java
@@ -3,5 +3,11 @@ package linkedlistimplementation;
 public class LinkedListTest {
     public static void main(String[] args) {
         System.out.println("Welcome to linked list program");
+        LinkedList linkedList=new LinkedList();
+        linkedList.add(70);
+        linkedList.add(30);
+        linkedList.add(56);
+        linkedList.printList();
+
     }
 }
