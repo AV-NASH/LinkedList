db62a8e7e51697081c199c9cf0110905442bde5e
AV-NASH <avinash21213434@gmail.com>
2020-10-16 23:40:32 +0530

    Added method to insert element to linked list

--- src/linkedlistimplementation/LinkedList.java
+++ src/linkedlistimplementation/LinkedList.java
@@ -26,7 +26,22 @@ public class LinkedList<E> {
             tempNode.next=node;
         }
     }
+    public void insert(E exisitngData, E newData){
+        Node exisitngNode;
+        Node newNode=new Node(newData);
+        exisitngNode=head;
 
+        while(exisitngNode!=null){
+            if(exisitngNode.data.equals(exisitngData))
+                break;
+            exisitngNode=exisitngNode.next;
+        }
+        Node tempnode= exisitngNode.next;
+        exisitngNode.next=newNode;
+        newNode.next=tempnode;
+
+
+    }
 
     public void printList(){
         if(head==null) System.out.println("LinkedList empty");
--- src/linkedlistimplementation/LinkedListTest.java
+++ src/linkedlistimplementation/LinkedListTest.java
@@ -4,9 +4,9 @@ public class LinkedListTest {
     public static void main(String[] args) {
         System.out.println("Welcome to linked list program");
         LinkedList linkedList=new LinkedList();
-        linkedList.append(70);
-        linkedList.append(56);
-        linkedList.append(30);
+        linkedList.add(70);
+        linkedList.add(56);
+        linkedList.insert(56,30);
         linkedList.printList();
 
 
